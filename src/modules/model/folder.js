6b4c9441b279f98be53a2a6ccb4510536cccb8bc
CCPablo <32818594+CCPablo@users.noreply.github.com>
2020-12-05 12:58:43 +0100

    create folders object (#10)

---
+++ src/modules/model/folder.js
@@ -0,0 +1,49 @@
+import { File } from './file.js'
+
+export { Folder }
+
+const FOLDER_EXISTS_MSG = function(folderName) {
+    return `A folder with name ${folderName} already exists in directory`;
+}
+
+const FILE_EXISTS_MSG = function(fileName) {
+    return `A file with name ${fileName} already exists in directory`;
+}
+
+class Folder {
+    constructor(files = {}, folders = {}, shortcut = undefined) {
+        this.files = files;
+        this.folders = folders;
+        this.shortcut = shortcut;
+    }
+
+    addFolder = function(name, files = {}, folders = {}, shortcut = undefined) {
+        if(!this.hasFolder(name)) {
+            throw FOLDER_EXISTS_MSG(name);
+        }
+        this.folders[name] = new Folder(files, folders, shortcut);
+    }
+
+    addFile = function(name, content = '') {
+        if(!this.hasFile(name)) {
+            throw FILE_EXISTS_MSG(name);
+        }
+        this.files[name] = new File(name, content);
+    }
+
+    getFolderNames = function() {
+        return Object.keys(this.folders);
+    }
+
+    getFileNames = function() {
+        return Object.keys(this.files);
+    }
+
+    hasFolder = function(fileName) {
+        return !Object.keys(this.files).includes(fileName);
+    }
+
+    hasFile = function(folderName) {
+        return !Object.keys(this.folders).includes(folderName);
+    }
+}
---
+++ src/modules/state/folders.js
@@ -0,0 +1,110 @@
+/*
+let rootFolder = {
+    folders: {
+        {
+            folders: {
+                {
+                    folders: [],
+                    files: []
+                },
+                {
+                    folders: [],
+                    files: []
+                }
+            },
+            files: {
+                {
+                    name: 'file1.js',
+                    content: 'Contenido del fichero 1'
+                }
+            }
+        },
+        {
+            folders: {
+                {
+                    folders: [],
+                    files: []
+                },
+                {
+                    folders: [],
+                    files: [
+                        {
+                            name: 'file1.txt',
+                            content: 'gola'
+                        }
+                    ]
+                }
+            },
+            files: {
+                {
+                    name: 'file1.js',
+                    content: 'Contenido del fichero 1'
+                }
+            }
+        }
+    },
+    files: {
+        {
+            name: 'file2.js',
+            content: 'Contenido del fichero 2'
+        }
+    {
+}
+*/
+
+import { Folder } from '../model/folder.js'
+
+export { getActiveFolder, enterFolder, exitFolder, getPath }
+
+let rootFolder = new Folder();
+
+let currentPath = [];
+
+function getActiveFolder() {
+    return currentPath.reduce((parent, child) =>  parent.folders[child], rootFolder)
+}
+
+function enterFolder(name) {
+    currentPath.push(name);
+}
+
+function exitFolder() {
+    currentPath.pop();
+}
+
+function getPath() {
+    return `/${currentPath.join('/')}`;
+}
+
+////
+
+logState();
+logAction('add 3 files to root folder')
+
+getActiveFolder().addFile('file1.js')
+getActiveFolder().addFile('file2.js')
+getActiveFolder().addFile('file3.js')
+
+logState();
+logAction('add folder new folder and set active')
+
+getActiveFolder().addFolder('new folder')
+enterFolder('new folder')
+
+logState();
+logAction('add file inside folder')
+
+getActiveFolder().addFile('file4.js')
+
+logState();
+
+function logState() {
+    console.log('\n')
+    console.log(getActiveFolder());
+    console.log(getPath())
+    console.log('\n')
+}
+
+function logAction(action) {
+    console.log('--> ' + action);
+}
