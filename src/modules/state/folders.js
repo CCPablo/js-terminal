ed01e1dabcf0c1bfb75e730b04b45f6b665625e8
CCPablo <pablocaravacacalvo@gmail.com>
2020-12-05 01:47:47 +0100

    rewrite rootFolder example

--- src/modules/state/folders.js
+++ src/modules/state/folders.js
@@ -1,24 +1,18 @@
 /*
 let rootFolder = {
-    path: [],
-    shortcut: 'root',
-    folders: }
+    folders: {
         {
-            path: ['folder1'],
             folders: {
                 {
-                    path: ['folder1', 'folder1_1'],
-                    shortcut: 'ff1',
                     folders: [],
                     files: []
                 },
                 {
-                    path: ['folder1', 'folder2_1'],
                     folders: [],
                     files: []
                 }
             },
-            files: }
+            files: {
                 {
                     name: 'file1.js',
                     content: 'Contenido del fichero 1'
@@ -26,21 +20,17 @@ let rootFolder = {
             }
         },
         {
-            path: ['folder2'],
-            shortcut: 'f2',
             folders: {
                 {
-                    path: ['folder2', 'folder1_1'],
                     folders: [],
                     files: []
                 },
                 {
-                    path: ['folder2', 'folder1_1'],
                     folders: [],
                     files: [
                         {
-                            path: '/root/folder1/folder1_2/gola.txt',
-                            conten: 'gola'
+                            name: 'file1.txt',
+                            content: 'gola'
                         }
                     ]
                 }
