6a42a472b8e3beb70c330196c69e1801a082e011
Marcel <googlillo@gmail.com>
2020-12-05 19:47:53 +0100

    Added base structure to fetch input from user.

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 import {commandsList, runCommand} from './src/modules/data/commands.js'
 
 
-
 console.log(runCommand('pwd', 2))
 
+let input = document.querySelector('.terminal__input')
+
+input.addEventListener('keydown', e => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        fetchInput(e.target.value)
+        input.value = '';
+    }
+});
+
+function fetchInput(val) {
+    if (input.value !== '') {
+        formatInput(val)
+    }
+}
+
+fetchInput();
+
+function formatInput(a) {
+    let splited = a.split(' ');
+    switch (splited[0]) {
+        case 'ls':
+            console.log('list folders')
+            break;
+        case 'cd':
+            break;
+        case 'pwd':
+            break;
+        case 'mkdir':
+            break;
+        case 'echo':
+            break;
+        case 'touch':
+            break;
+        case 'cat':
+            break;
+        case 'rm':
+            break;
+        case 'mv':
+            break;
+        case 'clear':
+            break;
+    }
+}
