fa6f843a88f1dc8875cc69101cb3b538fe72ec39
CCPablo <pablocaravacacalvo@gmail.com>
2020-12-10 21:49:27 +0100

    solution

--- src/modules/state/folders.js
+++ src/modules/state/folders.js
@@ -5,11 +5,6 @@ let rootFolder = new Folder();
 
 let absolutPath = [];
 
-/*
-    - returns the relative folder based on current path
-    - throws error if folder does not exist
-*/
-
 function getFolder(relativePath = "") {
     const relativePathPointer = getRelativePathPointer(relativePath);
     return extractFolder(relativePathPointer);
