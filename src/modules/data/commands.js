59ca60541ed2d60a22537309a7a70cd6f4000eb8
Leyber91 <leyber.91.2@gmail.com>
2020-12-11 16:50:51 +0100

    essential funcionality of cmdMode accomplished

--- src/modules/data/commands.js
+++ src/modules/data/commands.js
@@ -91,19 +91,18 @@ const help = new Command(
     }
 )
 
-
+/// Generate to return all commands man and help
 const man = new Command(
     'man - an interface to the system reference manuals.',
     manMan.All,
     (argumentList, parameterList) => {
         if (argumentList.length === 0) {
             for ( let command in commandsList) {
-                const descriptions = commandsList[command].manRef
+                const descriptions = commandsList[command].manRef;
                 return descriptions;
             }
         } else {
-            const descriptions = commandsList[argumentList].manRef;
-            return descriptions;
+            return commandsList[argumentList].manRef;
         }
     }
 )
@@ -129,16 +128,30 @@ const cmdMode = new Command(
     'cmdMode - changes the terminal Mode',
     '',
     (argumentList, parameterList) => {
+        let setTheme  = function (themeName) {
+            document.documentElement.className = themeName ;
+            
+        }
+
+        
+/// TO DO
+/// Equalize class name to input in function
+/// Extract to module where command list take place
+
         if (argumentList == 'Windows'){
+            
+            setTheme('windowsPowerShell');
             console.log ('error 404')
             return 'Changed to Powershell'
         }
 
         else if (argumentList == "Apple") {
+            setTheme('appleCommandStyle');
             console.log ('No java accepted')
             return 'Changed to Apple Command prompt'
         }
         else if (argumentList == "Linux") {
+            setTheme('linuxCommandStyle');
             console.log ('I am free')
             return 'Changed to Linux Command prompt'
         }
